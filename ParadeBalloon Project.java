19ad88cdb26e3959ce187be5a4a490d5db60eec3
Lordhoobla <45267209+Lordhoobla@users.noreply.github.com>
2020-11-10 14:02:28 -0600

    Create ParadeBalloon Project.java

---
+++ ParadeBalloon Project.java
@@ -0,0 +1,8 @@
+//NOTE: THIS IS NOT IN THE EDHESIVE PRODUCT
+import java.util.Scanner;
+public Main{
+  public static void main(String[] args){
+    ParadeBalloon x=new ParadeBalloon();
+    
+  }
+}
